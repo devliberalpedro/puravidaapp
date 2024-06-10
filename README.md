959d937b3150ceb89075092eb5408cbb8e50d7a5
devliberalpedro <dev.liberalpedro@gmail.com>
2024-06-10 00:02:31 -0300

    .

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 2. Clonar o projeto do github na IDE de preferência
    https://github.com/devliberalpedro/puravidaapp
 3. Executar
+
+==> Usuário padrão: puravidaapp
+    Caso insira um usuário inexistente na base de dados, será permitido cadastar um novo
+    
+==> Senha padrão: puravidaapp
--- professionals.py
+++ professionals.py
@@ -141,56 +141,47 @@ def search_by_city(assets_path):
     main.screen_clear()
     print('>> Pesquisa de profissionais (por cidade) <<\n')
 
-    # Recebe os estabelecimentos já cadastrados
-    placesbase = read_professionals(assets_path)
+    # Recebe os profissionais já cadastrados
+    professionalsbase = read_professionals(assets_path)
 
-    search_description = input('Qual profissional deseja pesquisar? ')
+    search_description = input('Qual a cidade do profissional? ')
 
-    # Lista auxiliar para verificar se o estabelecimento já existe
-    # Executará um laço para inserir as ocorrências deste estabelecimento
-    # Caso o estabelecimento não tenha ocorrência, pergunta se deve realizar o registro
+    # Executará um laço para inserir as ocorrências
     places_list = []
 
-    for idx in range(len(placesbase)):
-        places_list.append(placesbase[idx].get('NAME'))
+    for idx in range(len(professionalsbase)):
+        if professionalsbase[idx].get('CITY') == search_description:
+            places_list.append(professionalsbase[idx].get('NAME'))
 
     # Busca por ocorrências com a substring da descrição na base de produtos
     found = [ idx for idx in places_list if search_description.capitalize() in idx ]
 
     if len(found) == 0:
-        print('\nNenhum estabelecimento encontrado com este nome')
+        print('\nNenhum profissional encontrado nesta cidade')
     else:
-        print('\nEstabelecimentos encontrados: ')
+        print('\nProfissionais encontrados: ')
         
         for idx in range(len(found)):
             print(f'     {idx + 1}. {found[idx]}')
 
-        selected = int(input('\nQual estabelecimento deseja mais informações? ')) -1
+        selected = int(input('\nQual profissional deseja mais informações? ')) -1
 
-        place_index = places_list.index(found[selected])
+        professional_index = places_list.index(found[selected])
 
-        print(f'\nNome: {placesbase[place_index].get('NAME')}')
-        print(f'Tipo: {placesbase[place_index].get('TYPE')}')
-        print(f'Telefone: {placesbase[place_index].get('PHONE')}')
-        print(f'Email: {placesbase[place_index].get('EMAIL')}')
-        print(f'Endereço: {placesbase[place_index].get('ADDRESS')}')
-        print(f'Cidade: {placesbase[place_index].get('CITY')}')
+        print(f'\nNome: {professionalsbase[professional_index].get('NAME')}')
+        print(f'Especialização: {professionalsbase[professional_index].get('EXPERTISE')}')
+        print(f'Telefone: {professionalsbase[professional_index].get('PHONE')}')
+        print(f'Email: {professionalsbase[professional_index].get('EMAIL')}')
 
         print('Redes socials:')
-        for idx, social in enumerate(placesbase[place_index].get('SOCIAL')):
+        for idx, social in enumerate(professionalsbase[professional_index].get('SOCIAL')):
             print(f'>> {idx + 1}: {social}')
-        
-        print(f'Biografia: {placesbase[place_index].get('BIO')}')
-        print(f'Avaliação de estrelas: {placesbase[place_index].get('STARS')}')
-        print(f'Total de avaliações: {placesbase[place_index].get('NUMBER STARS')}')
 
-        print('Avaliações:')
-        for idx, review in enumerate(placesbase[place_index].get('REVIEWS')):
-            print(f'>> {idx + 1}: {review}')
-        
-        print('Serviços:')
-        for idx, service in enumerate(placesbase[place_index].get('SERVICES')):
-            print(f'>> {idx + 1}: {service}')
+        print(f'Endereço: {professionalsbase[professional_index].get('ADDRESS')}')
+        print(f'Cidade: {professionalsbase[professional_index].get('CITY')}')
+
+        print(f'Site: {professionalsbase[professional_index].get('SITE')}')
+        print(f'Biografia: {professionalsbase[professional_index].get('BIO')}')
     
     main.press_continue()
 
